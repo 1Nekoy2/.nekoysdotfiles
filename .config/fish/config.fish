6b546e8c496b9b50b4d9ea80a77f8d2a8f4224e6
Yuki <fox12647363@gmail.com>
2024-03-15 11:28:13 +0100

    new alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Supresses fish's intro message and start something cool :3
     set fish_greeting
        fortune | cowsay -f kitty | lolcat
-
+       echo 
     # automaticaly start tmux
 #   if status is-interactive
 #   and not set -q TMUX
@@ -13,6 +13,7 @@ if status is-interactive
 #   fish_config theme save "Catppuccin Mocha"
 
     # aliases
+    alias nvi="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
     alias la="lsd -hA --group-dirs first"
     alias l="lsd"
     alias ll="lsd -hlA --group-dirs first"
