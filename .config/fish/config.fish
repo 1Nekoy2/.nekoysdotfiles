2e98853d3c5ee8e2d2033412d6c041c576d7cb4d
Yuki <fox12647363@gmail.com>
2024-03-15 18:43:54 +0100

    Major overhoul of tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,16 +4,16 @@ if status is-interactive
        fortune | cowsay -f kitty | lolcat
        echo 
     # automaticaly start tmux
-#   if status is-interactive
-#   and not set -q TMUX
-#       exec tmux
-#   end
+    if status is-interactive
+    and not set -q TMUX
+        exec tmux new-session -A -s main
+    end
 
-   #theme
-#   fish_config theme save "Catppuccin Mocha"
+    #theme
+#    fish_config theme save "Catppuccin Mocha"
 
     # aliases
-    alias nvi="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
+    alias nvi="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | nvim"
     alias la="lsd -hA --group-dirs first"
     alias l="lsd"
     alias ll="lsd -hlA --group-dirs first"
@@ -26,9 +26,9 @@ if status is-interactive
     
     #fuzzy finder themeing 
     set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
     #zoxide integration
     zoxide init --cmd cd fish | source
